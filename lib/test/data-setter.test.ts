b833467915b4ab40a5de214939d141adbd096cc8
toshiboy86 <toshibowie86@gmail.com>
2025-04-04 13:32:08 +0900

    feat: add Vitest configuration and country data tests

---
+++ lib/test/data-setter.test.ts
@@ -0,0 +1,71 @@
+import { afterAll, beforeEach, describe, expect, test, vi } from 'vitest'
+
+import { setData } from '../data-setter'
+import { ICountryData } from '@/data/country'
+import * as audioGetter from '../audio-getter'
+
+const data: Record<string, ICountryData[]> = {
+  b: [
+    {
+      country: 'b',
+      pronunciation: 'b',
+      characters: 'b',
+      flag: 'b',
+    },
+    {
+      country: 'a',
+      pronunciation: 'a',
+      characters: 'a',
+      flag: 'a',
+    },
+  ],
+  a: [
+    {
+      country: 'c',
+      pronunciation: 'c',
+      characters: 'c',
+      flag: 'c',
+    },
+  ],
+}
+
+describe('country data', () => {
+  let sortedData: Record<string, ICountryData[]>
+  let getCountryAudioSpy: any
+
+  beforeEach(() => {
+    getCountryAudioSpy = vi.spyOn(audioGetter, 'getCountryAudio')
+    getCountryAudioSpy.mockReturnValue('/audio/dummy.mp3')
+    sortedData = setData(data)
+  })
+
+  afterAll(() => {
+    getCountryAudioSpy.mockRestore()
+  })
+
+  describe('sort the continents by alphabetical order', () => {
+    test('should sort the continents by alphabetical order', () => {
+      expect(Object.keys(sortedData)).toEqual(['a', 'b'])
+    })
+
+    test('should sort the countries by alphabetical order', () => {
+      expect(sortedData['b'][0].country).toEqual('a')
+      expect(sortedData['b'][1].country).toEqual('b')
+    })
+
+    test('should add audio path to each country', () => {
+      expect(sortedData['a'][0].audiopath).toEqual('/audio/dummy.mp3')
+      expect(sortedData['b'][0].audiopath).toEqual('/audio/dummy.mp3')
+      expect(sortedData['b'][1].audiopath).toEqual('/audio/dummy.mp3')
+    })
+  })
+
+  describe('audio handling', () => {
+    test('should throw error when audio is not available', () => {
+      getCountryAudioSpy.mockReturnValue(undefined)
+      expect(() => setData(data)).toThrow(
+        `Audio data is not set for ${data['a'][0].country}. Please set audio file.`
+      )
+    })
+  })
+})
