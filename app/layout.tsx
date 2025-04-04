0317905b1058861905c93bf7778f892058b33ba1
toshiboy86 <toshibowie86@gmail.com>
2025-04-04 20:30:10 +0900

    temp commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,39 @@ import type React from 'react'
 import type { Metadata } from 'next'
 import './globals.css'
 
+// TO be env values
+// const url = 'https://sayethereum.com'
+// const image = 'https://sayethereum.com/og.png'
+const url = 'http://localhost:3000'
+const image = 'http://localhost:3000/og.png'
+
+const title =
+  'Say Ethereum - Discover how to pronounce Ethereum in different languages and countries by Geodework'
+const description =
+  'Learn how to pronounce Ethereum in different languages around the world. Geodework will pursue the geographic decentralization of Ethereum.'
+
 export const metadata: Metadata = {
-  title: 'Say Ethereum - Pronunciation Guide',
-  description:
-    'Learn how to pronounce Ethereum in different languages around the world in a cute Japanese style',
-  generator: 'v0.dev',
+  title,
+  description,
+  icons: {
+    icon: [{ url: '/favicon.ico', sizes: 'any' }],
+    apple: [
+      { url: '/apple-touch-icon.png', type: 'image/png', sizes: '180x180' },
+    ],
+  },
+  openGraph: {
+    title,
+    description,
+    type: 'website',
+    url,
+    images: [{ url: image }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images: [image],
+  },
 }
 
 export default function RootLayout({
@@ -20,5 +48,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-import './globals.css'
