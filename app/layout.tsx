10599d325de548acd409a22231adf19a6e0add97
toshiboy86 <toshibowie86@gmail.com>
2025-04-21 11:11:21 +0900

    feat: integrate Matomo analytics tool and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "./globals.css"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
+import Matomo from "@/components/AnalyticsTool"
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-comic min-h-screen flex flex-col">
+        <Matomo />
         <Header />
         {children}
         <Footer />
