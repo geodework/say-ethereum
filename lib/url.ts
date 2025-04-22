f1cbb08ba4581665784125b1e50f2e22b404a272
toshiboy86 <toshibowie86@gmail.com>
2025-04-22 11:38:34 +0900

    refactor: simplify base URL logic and use constant for production domain

--- lib/url.ts
+++ lib/url.ts
@@ -1,12 +1,11 @@
+const DOMAIN = "sayethereum.fun"
+
 export const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_DOMAIN) {
-    // Production domain (if set)
-    return `https://${process.env.NEXT_PUBLIC_DOMAIN}`
+  if (process.env.NEXT_PUBLIC_VERCEL_TARGET_ENV === "production") {
+    return `https://${DOMAIN}`
   }
-  if (process.env.VERCEL_URL) {
-    // Vercel deployment (preview or production)
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  if (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
   }
-  // Development
   return "http://localhost:3000"
 }
